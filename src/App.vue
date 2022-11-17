c88bdd97e298da20625181b8c7712e52310ced4d
Pratt <pratt.paredes@gmail.com>
2022-11-17 02:01:16 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import NavBarVue from './components/NavBar.vue';
+</script>
+
+<template>
+<NavBarVue/>
+</template>
+
+<style>
+
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav>
+    <div class="flex mx-auto justify-between p-5">
+      <figure>
+        <img src="" alt="LOGO" />
+      </figure>
+      <ul class="flex justify-between w-[500px]">
+        <li>
+          <a href="">Inicio</a>
+        </li>
+        <li>
+          <a href="">Inicio</a>
+        </li>
+        <li>
+          <a href="">Inicio</a>
+        </li>
+        <li>
+          <a href="">Inicio</a>
+        </li>
+        <li>
+          <a href="">Inicio</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
