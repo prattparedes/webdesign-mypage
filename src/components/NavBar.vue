e91a775eec70b6e83ccb37d6e80567c71caca3e8
Pratt <pratt.paredes@gmail.com>
2022-11-17 20:21:10 -0500

    navbar edits

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,24 +1,26 @@
 <template>
   <nav>
-    <div class="flex mx-auto justify-between p-5">
+    <div class="max-w-[1140px] flex mx-auto justify-between items-center p-5">
       <figure>
         <img src="" alt="LOGO" />
       </figure>
-      <ul class="flex justify-between w-[500px]">
-        <li>
-          <a href="">Inicio</a>
+      <ul class="flex justify-between w-[300px] items-center">
+        <li >
+          <a class="hover:bg-[lightgray] px-4 py-2" href="">Demos</a>
+        </li>
+        <li >
+          <a class="hover:bg-[lightgray] px-4 py-2" href="">Pricing</a>
+        </li>
+        <li >
+          <a class="hover:bg-[lightgray] px-4 py-2" href="">Contact</a>
+        </li>
+      </ul>
+      <ul class="flex space-x-4 items-center">
+        <li >
+          <a class="hover:bg-[lightgray] px-4 py-2" href="">Log In</a>
         </li>
         <li>
-          <a href="">Inicio</a>
-        </li>
-        <li>
-          <a href="">Inicio</a>
-        </li>
-        <li>
-          <a href="">Inicio</a>
-        </li>
-        <li>
-          <a href="">Inicio</a>
+          <button class="bg-[#242424] px-4 py-2 text-white "><a href="">Register for Free</a></button>
         </li>
       </ul>
     </div>
