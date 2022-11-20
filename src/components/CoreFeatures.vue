b567840d95c4b5467844e29fe94faa333233d375
Pratt <pratt.paredes@gmail.com>
2022-11-20 00:26:41 -0500

    little changes

--- src/components/CoreFeatures.vue
+++ src/components/CoreFeatures.vue
@@ -1,17 +1,9 @@
 <template>
-  <div id="features" class="h-[60vh] flex justify-center align-center flex-col items-center">
+  <div id="features" class="h-[70vh] flex justify-center align-center flex-col items-center bg-[cream]">
     <div class="text-center">
-      <h1 class="text-[40px]">Amazing web design features</h1>
+      <h1 class="text-[40px] font-extrabold">Amazing web design features</h1>
     </div>
-    <div class="flex flex-wrap max-w-1100 space-x-4 justify-center">
-      <div class="flex flex-wrap text-center w-[15%] h-[auto] p-5 border-[1px] border-[none] rounded-lg bg-[lightgray] justify-center items-center mt-4 hover:scale-[1.07] transition-all duration-300 ease-in-out">
-        <div class="flex flex-col h-[40%] w-[50%] items-center justify-center">
-          <font-awesome-icon icon="fa-solid fa-user-secret" class="text-[60px]"/>
-          <h3 class="text-3xl mt-2">
-              Responsive Design
-          </h3>
-        </div>
-      </div>
+    <div class="flex flex-wrap max-w-1100 space-x-4 justify-center mt-8">
       <div class="flex flex-wrap text-center w-[15%] h-[auto] p-5 border-[1px] border-[none] rounded-lg bg-[lightgray] justify-center items-center mt-4 hover:scale-[1.07] transition-all duration-300 ease-in-out">
         <div class="flex flex-col h-[40%] w-[50%] items-center justify-center">
           <font-awesome-icon icon="fa-solid fa-user-secret" class="text-[60px]"/>
